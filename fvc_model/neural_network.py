bb175fdb945bba8a953b8577904b6c0f71b1f088
Shashank Manjunath <manjuns@bu.edu>
2020-11-10 18:48:24 -0500

    Added neural net and training method

---
+++ fvc_model/neural_network.py
@@ -0,0 +1,41 @@
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.models import Model
+from tensorflow.keras import layers
+from segmentation import models
+
+
+
+def nn_model(freeze_backbone=False):
+    backbone = models.BCDU_net_D3(input_size=(None, 512,512))
+
+    if freeze_backbone:
+        backbone.trainable = False
+
+    bc_1 = layers.Conv2D(32, 3, activation="relu", padding="same", kernel_initializer="he_normal")(backbone.output)
+    mp_1 = layers.MaxPooling2D(pool_size=(3, 3))(bc_1)
+
+    bc_2 = layers.Conv2D(32, 3, activation="relu", padding="same", kernel_initializer="he_normal")(mp_1)
+    mp_2 = layers.MaxPooling2D(pool_size=(3, 3))(bc_2)
+
+    bc_3 = layers.Conv2D(32, 3, activation="relu", padding="same", kernel_initializer="he_normal")(mp_2)
+    mp_3 = layers.MaxPooling2D(pool_size=(3, 3))(bc_3)
+
+    bc_4 = layers.Conv2D(32, 3, activation="relu", padding="same", kernel_initializer="he_normal")(mp_3)
+    mp_4 = layers.MaxPooling2D(pool_size=(3, 3))(bc_4)
+
+    flat = layers.Flatten()(mp_4)
+    fc_1 = layers.Dense(512, activation="relu")(flat)
+
+    categorical_input = Input(shape=(4,))
+    cat_1 = Dense(32, activation="relu")(categorical_input)
+    cat_2 = Dense(16, activation="relu")(cat_1)
+    cat_3 = Dense(8, activation="relu")(cat_2)
+
+    combined = layers.concatenate([fc_1, cat_3])
+
+    output_1 = Dense(32, activation="relu")(combined)
+    output_2 = Dense(1, activation="softmax")(output_1)
+
+    model = Model(inputs=[backbone.input, categorical_input], outputs=output_2)
+    return model
+
---
+++ train.py
@@ -0,0 +1,101 @@
+from csv_reader.csv_to_dict import csv_to_dict
+from tensorflow.keras.optimizers import Adam
+from fvc_model.neural_network import nn_model
+from loader.loader import load_scan
+from tqdm import tqdm
+
+import tensorflow as tf
+import numpy as np
+import os
+
+
+sex_to_int = {
+    "Male": 0,
+    "Female": 1,
+}
+
+
+smoking_status_to_int = {
+    "Never": 0,
+    "Ex-smoker": 1,
+    "Currently": 2,
+}
+
+
+class ModelTrainer(object):
+    def __init__(self, model: tf.keras.models.Model, run_name: str):
+        self.model = model
+        self.run_name = run_name
+
+    def train(self, lr: float, train_data: dict, test_data: dict, epochs: int=100):
+        self.model.compile(optimizer=Adam(lr=lr), loss='binary_crossentropy', metrics=['accuracy'])    
+        history = self.model.fit(
+            train_data["data"], train_data["label"], 
+            batch_size=1, 
+            epochs=epochs,
+            validation_data=(test_data["data"], test_data["label"])
+        )
+        return
+
+    def eval(self, test_arr: np.ndarray):
+        predictions = model.predict(test_arr)
+        return predictions
+
+
+def load_dataset(scan_data_dir, csv_dir):
+    dataset = {"data": [[], []], "label": []}
+    csv_data = csv_to_dict(csv_dir)
+
+    if "train" in scan_data_dir:
+        csv_data = csv_data[0]
+    elif "test" in scan_data_dir:
+        csv_data = csv_data[1]
+
+    for pt_idx, patient_id in enumerate(tqdm(os.listdir(scan_data_dir))):
+        scan_data = load_scan(os.path.join(scan_data_dir, patient_id))
+        scan_data = scan_data / 255.
+        scan_data = np.expand_dims(scan_data, axis=0)
+
+        pt_data = csv_data[patient_id]
+
+        for idx, _ in enumerate(pt_data["Weeks"]):
+            dataset["data"][0].append(scan_data)
+
+            data_pt = [
+                pt_data["Weeks"][idx],
+                pt_data["Age"][0],
+                float(sex_to_int[pt_data["Sex"][0]]),
+                float(smoking_status_to_int[pt_data["Smoking Status"][idx]]),
+            ]
+            dataset["data"][1].append(data_pt)
+
+            #  data_pt = [scan_data, np.asarray([
+            #      pt_data["Weeks"][idx],
+            #      pt_data["Age"][0],
+            #      float(sex_to_int[pt_data["Sex"][0]]),
+            #      float(smoking_status_to_int[pt_data["Smoking Status"][idx]]),
+            #  ])]
+            #  dataset["data"].append(data_pt)
+            dataset["label"].append(pt_data["FVC"][idx])
+
+        if pt_idx > 5:
+            break
+
+    #  dataset = np.concatenate(dataset, axis=0)
+    return dataset
+
+
+if __name__ == "__main__":
+    model = nn_model()
+    train_dataset = load_dataset(
+        scan_data_dir="/projectnb/ece601/F-PuPS/kaggle/data/train",
+        csv_dir="/projectnb/ece601/F-PuPS/kaggle/data/",
+    )
+    test_dataset = load_dataset(
+        scan_data_dir="/projectnb/ece601/F-PuPS/kaggle/data/test",
+        csv_dir="/projectnb/ece601/F-PuPS/kaggle/data/",
+    )
+
+    trainer = ModelTrainer(model=model, run_name="tmp")
+    trainer.train(lr=1e-4, train_data=train_dataset, test_data=test_dataset, epochs=1)
+
